bfb8781c182b4fed14d167cf91cb9f710c0da026
Your Name <qdssl12138@163.com>
2023-03-06 23:11:55 +0800

    Initial commit

---
+++ src/test/java/com/example/bootweb/BootWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bootweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
