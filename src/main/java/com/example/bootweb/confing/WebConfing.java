bdec0449e3368eba7bbd01f38452a7cce8c2a1f5
zzm <qdssl12138@163.com>
2023-03-11 09:15:22 +0800

    更新

--- src/main/java/com/example/bootweb/confing/WebConfing.java
+++ src/main/java/com/example/bootweb/confing/WebConfing.java
@@ -1,13 +1,21 @@
 package com.example.bootweb.confing;
 
 import com.example.bootweb.bean.Pet;
+import com.example.bootweb.converter.GuiguMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.accept.HeaderContentNegotiationStrategy;
+import org.springframework.web.accept.ParameterContentNegotiationStrategy;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.util.UrlPathHelper;
+
+import java.util.*;
 
 import static org.springframework.util.StringUtils.hasLength;
 
@@ -27,10 +35,39 @@ public class WebConfing implements WebMvcConfigurer {
     }
 
 
+
+
     //方式二
     @Bean
     public WebMvcConfigurer webMvcConfigurer(){
         return new WebMvcConfigurer() {
+
+
+            /**
+             * 自定义协商策略
+             * @param configurer
+             */
+            @Override
+            public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+                Map<String, MediaType> mediaTypeMap = new HashMap<>();
+                mediaTypeMap.put("json",MediaType.APPLICATION_JSON);
+                mediaTypeMap.put("xml",MediaType.APPLICATION_XML);
+                mediaTypeMap.put("gg",MediaType.parseMediaType("application/x-guigu"));
+                //指定支持解析哪些参数对应得媒体类型
+                ParameterContentNegotiationStrategy parameterStrategy = new ParameterContentNegotiationStrategy(mediaTypeMap);
+
+                HeaderContentNegotiationStrategy headeStrategy = new HeaderContentNegotiationStrategy();
+
+
+                configurer.strategies(Arrays.asList(parameterStrategy,headeStrategy));
+            }
+
+            //配置内容协商功能
+            @Override
+            public void extendMessageConverters(List<HttpMessageConverter<?>> converters){
+                converters.add(new GuiguMessageConverter());
+            }
+
             @Override
             public void configurePathMatch(PathMatchConfigurer configurer) {
                 UrlPathHelper urlPathHelper = new UrlPathHelper();
--- src/main/java/com/example/bootweb/controller/ResponseTestController.java
+++ src/main/java/com/example/bootweb/controller/ResponseTestController.java
@@ -10,6 +10,18 @@ import java.util.Date;
 @Controller
 public class ResponseTestController {
 
+    /**
+     * 浏览器发送请求直接返回xml [application/xml]
+     * ajax请求 返回json      [application/json]
+     * 应用客户端请求返回自定义协议数据  [application/]
+     *
+     * 步骤
+     * 添加自定义MessageConverter进入系统底层
+     * 系统底层统计出所有MessageConverter能操作哪些类型
+     * 客户端协商
+     *
+     * @return
+     */
     @ResponseBody
     @GetMapping("/test/person")
     public Person getPerson(){
